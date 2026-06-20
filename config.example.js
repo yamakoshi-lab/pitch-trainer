8d4ff951324347670ad6cb1257d231e873dced84
yamakoshi-lab <249239955+yamakoshi-lab@users.noreply.github.com>
2026-06-20 20:30:03 +0900

    Configure gitignore for local config.js and add config template

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// ローカル開発用設定ファイルテンプレート
+// 1. このファイルをコピーして「config.js」という名前のファイルを作成してください。
+// 2. 作成した config.js の中に、あなたのGASウェブアプリURLを貼り付けてください。
+// ※ config.js は .gitignore により、GitHubへのプッシュ対象から自動的に除外されます。
+
+window.LOCAL_GAS_URL = "https://script.google.com/macros/s/ここにあなたのGASウェブアプリURLを貼り付け/exec";
